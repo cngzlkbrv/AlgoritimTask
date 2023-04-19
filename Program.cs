4e881e40e30687a828b35a951e462a4c77bd1637
Fehm1 <fehminhuseynov03@gmail.com>
2023-04-19 15:30:57 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> reqem = new List<int>();
+while (true)
+{
+    Console.WriteLine("Y-daxil et eks halda cix");
+    string command = Console.ReadLine();
+    switch (command)
+    {
+        case "Y":
+            Console.WriteLine("Ededi dqaxil et:");
+            int eded = int.Parse(Console.ReadLine());
+            reqem.Add(eded);
+            break;
+        default:
+            int cem = 0;
+            //foreach(int i in reqem)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         cem += i;
+            //     }
+            //     Console.WriteLine(cem);
+            // }
+            List<int> newlist = reqem.FindAll(x => x % 2 == 0);
+                        newlist.ForEach(x => cem += x);
+            return 0;
+    }
+}
